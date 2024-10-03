3be488112b4aef1c54ff9f68f3cc12970e7fee2b
Benjy Weinberger <benjyw@gmail.com>
2024-10-02 17:52:32 -0700

    WIP

--- src/python/pants/backend/python_new/goals.py
+++ src/python/pants/backend/python_new/goals.py
@@ -1,12 +1,13 @@
 # Copyright 2024 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
-from pants.backend.python_new.interpreter import generate_complete_platform, InterpreterConstraint
-
-from pants.engine.fs import Workspace
-
+from pants.backend.python_new.interpreter import generate_complete_platform, InterpreterConstraint, \
+    complete_platform_path
+from pants.backend.python_new.subsystems import PythonSettings
 from pants.engine.console import Console
+from pants.engine.fs import Workspace
 from pants.engine.goal import Goal, GoalSubsystem, LineOriented
+from pants.engine.internals.selectors import concurrently
 from pants.engine.rules import collect_rules, goal_rule
 
 
@@ -25,9 +26,15 @@ async def inspect_interpreters(
     console: Console,
     workspace: Workspace,
     inspect_interpreters_subsystem: InspectInterpretersSubsystem,
+    python_settings: PythonSettings,
 ) -> InspectInterpreters:
-    complete_platform = await generate_complete_platform(InterpreterConstraint("==3.11.9"))
-    workspace.write_digest(complete_platform.digest)
+    complete_platforms = await concurrently(
+        generate_complete_platform(
+            complete_platform_path(name, python_settings),
+            InterpreterConstraint(constraint_str)) for name, constraint_str in python_settings.interpreters.items()
+    )
+    for complete_platform in complete_platforms:
+        workspace.write_digest(complete_platform.digest)
     return InspectInterpreters(exit_code=0)
 
 
--- src/python/pants/backend/python_new/interpreter.py
+++ src/python/pants/backend/python_new/interpreter.py
@@ -1,10 +1,13 @@
 # Copyright 2024 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 import logging
+import os
 from dataclasses import dataclass
 
 from pants.backend.python.util_rules.pex_cli import PexCliProcess
+from pants.backend.python_new.subsystems import PythonSettings
 from pants.engine.internals.native_engine import Digest
+from pants.engine.platform import Platform
 from pants.engine.process import fallible_to_exec_result_or_raise
 from pants.engine.rules import rule, collect_rules, implicitly
 
@@ -24,21 +27,26 @@ class CompletePlatform:
     digest: Digest
 
 
+def complete_platform_path(name: str, python_settings: PythonSettings) -> str:
+    local_platform = Platform.create_for_localhost()
+    return os.path.join(python_settings.settings_dir, "interpreters", f"{name}.{local_platform.name}.json")
+
+
 @rule
-async def generate_complete_platform(ic: InterpreterConstraint) -> CompletePlatform:
+async def generate_complete_platform(output_path: str, ic: InterpreterConstraint) -> CompletePlatform:
     pex_args = ["interpreter", "inspect", "--markers", "--tags", "--indent", "2",
                 "--interpreter-constraint", ic.constraint,
-                "--output", "complete_platform.json"]
-    logging.warning(f"RUNNING pex {' '.join(pex_args)}")
+                "--output", output_path]
     pex_proc = PexCliProcess(
         subcommand=pex_args,
         extra_args=tuple(),
         description=f"Generate complete platforms for Python {ic}",
-        output_files=("complete_platform.json",)
+        output_files=(output_path,)
     )
     result = await fallible_to_exec_result_or_raise(**implicitly(
         {pex_proc: PexCliProcess}
     ))
+
     return CompletePlatform(digest=result.output_digest)
 
 
