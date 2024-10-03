9ec4ee0f100d2d29ba0e4fb22bcc17ea81b24c5c
Benjy Weinberger <benjyw@gmail.com>
2024-10-02 17:52:32 -0700

    WIP

--- src/python/pants/backend/python_new/subsystems.py
+++ src/python/pants/backend/python_new/subsystems.py
@@ -1,21 +1,36 @@
 # Copyright 2024 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
-from pants.option.option_types import DictOption
+from pants.option.option_types import DictOption, StrOption
 from pants.option.subsystem import Subsystem
 from pants.util.strutil import softwrap
 
 
-class PythonConfig(Subsystem):
+class PythonSettings(Subsystem):
+    options_scope = "python-new"
+    help = "Python-related settings for the repo."
+
+    settings_dir = StrOption(
+        default = "python-settings",
+        help = softwrap(
+            """
+            Python-related settings files are generated under this directory.
+            
+            These files are intended to be checked in to the repo.
+            """
+        )
+    )
+
     interpreters = DictOption[str](
         default=None,
         help=softwrap(
             """
-            The Python interpreters in use in your codebase.
+            The Python interpreters in use in your repo.
 
-            Must be a list of fully-specified Python versions, such as
-            'CPython==3.11.6' or 'PyPy==pypy3.10-7.3.12'. Wildcards are not allowed.
+            Must be a map from logical name, such as 'py3.11' to fully-specified
+            Python version, such as 'CPython==3.11.6' or 'PyPy==pypy3.10-7.3.12'. 
+            Wildcards are not allowed.
             """
         ),
-        metavar="<interpreter version>",
+        metavar="<interpreter mapping>",
     )
