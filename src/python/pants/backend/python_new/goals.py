9866085b974cab134e4836f92bb5b41cfc57c484
Benjy Weinberger <benjyw@gmail.com>
2024-10-03 22:15:35 -0700

    WIP

--- src/python/pants/backend/python_new/goals.py
+++ src/python/pants/backend/python_new/goals.py
@@ -2,13 +2,19 @@
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
 from pants.backend.python_new.interpreter import generate_complete_platform, InterpreterConstraint, \
-    complete_platform_path
+    get_complete_platform_path
 from pants.backend.python_new.subsystems import PythonSettings
 from pants.engine.console import Console
 from pants.engine.fs import Workspace
 from pants.engine.goal import Goal, GoalSubsystem, LineOriented
+from pants.engine.internals.native_engine import MergeDigests
 from pants.engine.internals.selectors import concurrently
+from pants.engine.intrinsics import merge_digests, get_digest_contents, get_digest_entries
 from pants.engine.rules import collect_rules, goal_rule
+
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class InspectInterpretersSubsystem(LineOriented, GoalSubsystem):
@@ -30,11 +36,14 @@ async def inspect_interpreters(
 ) -> InspectInterpreters:
     complete_platforms = await concurrently(
         generate_complete_platform(
-            complete_platform_path(name, python_settings),
+            get_complete_platform_path(name, python_settings),
             InterpreterConstraint(constraint_str)) for name, constraint_str in python_settings.interpreters.items()
     )
-    for complete_platform in complete_platforms:
-        workspace.write_digest(complete_platform.digest)
+    merged_digest = await merge_digests(MergeDigests(complete_platform.digest for complete_platform in complete_platforms))
+    workspace.write_digest(merged_digest)
+    digest_entries = await get_digest_entries(merged_digest)
+    for digest_entry in digest_entries:
+        logger.info(f"Wrote {digest_entry.path}")
     return InspectInterpreters(exit_code=0)
 
 
--- src/python/pants/backend/python_new/interpreter.py
+++ src/python/pants/backend/python_new/interpreter.py
@@ -1,12 +1,14 @@
 # Copyright 2024 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
-import logging
+
 import os
 from dataclasses import dataclass
 
 from pants.backend.python.util_rules.pex_cli import PexCliProcess
 from pants.backend.python_new.subsystems import PythonSettings
+from pants.engine.fs import EMPTY_DIGEST, PathGlobs
 from pants.engine.internals.native_engine import Digest
+from pants.engine.intrinsics import path_globs_to_digest
 from pants.engine.platform import Platform
 from pants.engine.process import fallible_to_exec_result_or_raise
 from pants.engine.rules import rule, collect_rules, implicitly
@@ -18,16 +20,27 @@ class InterpreterConstraint:
 
 
 @dataclass(frozen=True)
-class InterpreterVersion:
-    version: str
-
-
-@dataclass(frozen=True)
 class CompletePlatform:
     digest: Digest
 
 
-def complete_platform_path(name: str, python_settings: PythonSettings) -> str:
+@dataclass(frozen=True)
+class Interpreter:
+    complete_platform: CompletePlatform
+
+
+@rule
+async def get_interpreter(name: str, python_settings: PythonSettings) -> Interpreter:
+    complete_platform_path = get_complete_platform_path(name, python_settings)
+    complete_platform_digest = await path_globs_to_digest(PathGlobs([]))
+    if complete_platform_digest == EMPTY_DIGEST:
+        raise Exception(f"No complete platform file found at {complete_platform_path}. Run `pants inspect-interpreters` to create one.")
+    else:
+        complete_platform = CompletePlatform(complete_platform_digest)
+    return Interpreter(complete_platform)
+
+
+def get_complete_platform_path(name: str, python_settings: PythonSettings) -> str:
     local_platform = Platform.create_for_localhost()
     return os.path.join(python_settings.settings_dir, "interpreters", f"{name}.{local_platform.name}.json")
 
